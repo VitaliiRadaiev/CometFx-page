48468cd5d3532164c77f9341ef85ac209742dd09
VitaliiRadaiev <hf.chelenger@gmail.com>
2020-11-09 20:55:56 +0300

    add page

--- js/app.js
+++ js/app.js
@@ -670,6 +670,20 @@ function selects_update_all() {
 
 };
 
+function cardNewsHandler(){
+	let cards = document.querySelectorAll('.card-news');
+	if(cards.length) {
+		cards.forEach(item => {
+			let text = item.querySelector('.card-news__text');
+			if(text.innerText.length > 162) {
+				text.innerText = [...text.innerText].slice(0, 162).join('') + '...';
+			}
+		})
+	}
+};
+
+cardNewsHandler();;
+
 // === aside handler ==================================================================
 // document.body.addEventListener('click', (e) => {
 // 	console.log(e.target)
